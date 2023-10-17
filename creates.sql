58abbf55fb0ad5d6baf56f98d7e124006e79cbeb
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-10-17 11:25:46 -0300

    Update creates.sql

--- creates.sql
+++ creates.sql
@@ -2,32 +2,32 @@ CREATE SCHEMA meu_banco;
 USE meu_banco;
 
 CREATE TABLE info_pessoais_aluno (
-	cpf VARCHAR(15) PRIMARY KEY,
-	nome VARCHAR(40) NOT NULL,
-	tel_celular_resp VARCHAR(15) NOT NULL,
-	tel_residencial_resp VARCHAR(15),
+    cpf VARCHAR(15) PRIMARY KEY,
+    nome VARCHAR(40) NOT NULL,
+    tel_celular_resp VARCHAR(15) NOT NULL,
+    tel_residencial_resp VARCHAR(15),
     email_resp VARCHAR(40) NOT NULL,
     status_info BOOL DEFAULT 1
 );
 
 CREATE TABLE aluno (
-	num_matricula_aluno BIGINT AUTO_INCREMENT PRIMARY KEY,
+    num_matricula_aluno BIGINT AUTO_INCREMENT PRIMARY KEY,
     data_inicio_aluno DATE NOT NULL,
-	cpf_aluno VARCHAR(15) UNIQUE NOT NULL,
+    cpf_aluno VARCHAR(15) UNIQUE NOT NULL,
     email_aluno VARCHAR(40),
     status_aluno BOOL DEFAULT 1,
     CONSTRAINT fk_cpf_aluno FOREIGN KEY (cpf_aluno) REFERENCES info_pessoais_aluno(cpf)
 );
 
 CREATE TABLE curso (
-	codigo_curso BIGINT AUTO_INCREMENT PRIMARY KEY,
+    codigo_curso BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome_curso VARCHAR(50) NOT NULL,
     duracao_curso INT NOT NULL CHECK (duracao_curso >= 0),
     curso_status BOOL DEFAULT 1
 );
 
 CREATE TABLE turma (
-	codigo_tur BIGINT AUTO_INCREMENT PRIMARY KEY,
+    codigo_tur BIGINT AUTO_INCREMENT PRIMARY KEY,
     qtd_alunos_tur INT NOT NULL CHECK (qtd_alunos_tur >= 0),
     periodo_tur VARCHAR(30) NOT NULL,
     semestre_tur VARCHAR(30) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE turma (
 );
 
 CREATE TABLE professor (
-	codigo_prof BIGINT AUTO_INCREMENT PRIMARY KEY,
+    codigo_prof BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome_prof VARCHAR(40) NOT NULL,
     especialidade_prof VARCHAR(50) NOT NULL,
     data_admissao_prof DATE NOT NULL,
@@ -43,17 +43,17 @@ CREATE TABLE professor (
 );
 
 CREATE TABLE disciplina (
-	codigo_disc BIGINT AUTO_INCREMENT PRIMARY KEY,
-	nome_disc VARCHAR(50) NOT NULL,
-	ementa_disc TEXT NOT NULL,
-	status_disc BOOL DEFAULT 1
+    codigo_disc BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome_disc VARCHAR(50) NOT NULL,
+    ementa_disc TEXT NOT NULL,
+    status_disc BOOL DEFAULT 1
 );
 
 CREATE TABLE matricula (
-	numero_matricula BIGINT AUTO_INCREMENT,
-	codigo_curso BIGINT,
-	codigo_turma BIGINT,
-	matricula_status BOOL DEFAULT 1,
+    numero_matricula BIGINT AUTO_INCREMENT,
+    codigo_curso BIGINT,
+    codigo_turma BIGINT,
+    matricula_status BOOL DEFAULT 1,
     CONSTRAINT pk_matricula PRIMARY KEY (numero_matricula, codigo_curso, codigo_turma),
     CONSTRAINT fk_curso_matricula FOREIGN KEY (codigo_curso) REFERENCES curso(codigo_curso),
     CONSTRAINT fk_matricula_aluno FOREIGN KEY (numero_matricula) REFERENCES aluno(num_matricula_aluno),
@@ -62,7 +62,7 @@ CREATE TABLE matricula (
 
 
 CREATE TABLE aula (
-	codigo_aula BIGINT AUTO_INCREMENT PRIMARY KEY,
+    codigo_aula BIGINT AUTO_INCREMENT PRIMARY KEY,
     codigo_professor BIGINT NOT NULL,
     codigo_disciplina BIGINT NOT NULL,
     codigo_turma BIGINT NOT NULL,
