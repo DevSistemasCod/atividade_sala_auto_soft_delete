c1482a97d43c11e86313c2d4f317e51b32847032
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-10-17 11:27:12 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -8,11 +8,11 @@ VALUES
 
 INSERT INTO aluno (data_inicio_aluno, cpf_aluno, email_aluno)
 VALUES
-	('2022-01-10', '123.456.789-01', 'fernando1@email.com'),
-	('2022-06-01', '987.654.321-02', 'juliana22@email.com'),
-	('2021-01-15', '456.789.123-03', 'mariana32@email.com'),
-	('2023-01-10', '789.123.456-04', 'marcelo11@email.com'),
-	('2023-06-01', '234.567.890-05', 'luana123@email.com');
+	('2022-01-10', '123.456.789-01', 'fernando1@emaileduc.com'),
+	('2022-06-01', '987.654.321-02', 'juliana22@emaileduc.com'),
+	('2021-01-15', '456.789.123-03', 'mariana32@emaileduc.com'),
+	('2023-01-10', '789.123.456-04', 'marcelo11@emaileduc.com'),
+	('2023-06-01', '234.567.890-05', 'luana123@emaileduc.com');
 
 INSERT INTO disciplina (nome_disc, ementa_disc)
 VALUES
@@ -25,14 +25,14 @@ VALUES
 
 INSERT INTO professor (nome_prof, especialidade_prof, data_admissao_prof)
 VALUES
-	('Eduardo Nascimento', 'Tecnologia da Informação', '2023-01-15'),
+    ('Eduardo Nascimento', 'Tecnologia da Informação', '2023-01-15'),
     ('Matheus Michilino', 'Mecatrônica e Interfaces Robóticas', '2020-01-01'),
     ('Rafael Selvagio', 'Tecnologia da Informação', '2023-06-01'),
     ('Rafael Rizzi', 'Eng. Elétrica', '2022-01-01');
 
 INSERT INTO curso (nome_curso, duracao_curso)
 VALUES
-	('Técnico em Analise de Desenvolvimento de Sistemas', 18),
+    ('Técnico em Analise de Desenvolvimento de Sistemas', 18),
     ('Técnico em Mecatrônica', 24),
     ('Técnico em Eletrônica', 24),
     ('Técnico em Administração', 18),
@@ -41,7 +41,7 @@ VALUES
 
 INSERT INTO turma (qtd_alunos_tur, periodo_tur, semestre_tur)
 VALUES
-	(35, 'Manhã', '2 Semestre'),
+    (35, 'Manhã', '2 Semestre'),
     (35, 'Tarde', '2 Semestre'),
     (18, 'Manhã', '2 Semestre'),
     (20, 'Tarde', '4 Semestre'),
@@ -50,7 +50,7 @@ VALUES
 
 INSERT INTO aula (codigo_professor, codigo_disciplina, codigo_turma, data_aula)
 VALUES
-	(3, 1, 1, '2023-10-10'),
+    (3, 1, 1, '2023-10-10'),
     (1, 2, 2, '2023-10-11'),
     (2, 3, 1, '2023-10-12'),
     (1, 2, 1, '2023-10-13'),
